9abf4842c1c8b48b0889a7fee2f6d749793ba10c
rha6780 <rha4578@gmail.com>
2023-09-22 22:52:19 +0900

    feat: problem 관련 api 추가

--- src/main/java/com/example/algosolved/domain/problem/ProblemController.java
+++ src/main/java/com/example/algosolved/domain/problem/ProblemController.java
@@ -1,17 +1,37 @@
 package com.example.algosolved.domain.problem;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProblemController {
     @Autowired
     private ProblemRepository repository;
 
-    @RequestMapping("/api/v1/problem")
+    @Operation(summary = "get problem", description = "문제 정보 가져오기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    @Parameters({
+            @Parameter(name = "id", description = "문제 id", example = "1"),
+    })
+    @GetMapping("/api/v1/problem")
     public Iterable<Problem> getProblem(@RequestParam("id") long id) {
         return repository.findById(id);
+    }
+
+    @GetMapping("/api/v1/problems")
+    public Iterable<Problem> getProblems() {
+        return repository.findAll();
     }
 }
