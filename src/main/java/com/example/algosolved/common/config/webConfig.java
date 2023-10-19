a38a1914696bde04b6630552aad1c07e80b70f60
rha6780 <rha4578@gmail.com>
2023-10-19 14:42:20 +0900

    fix: spring 프론트 CORS 설정

--- src/main/java/com/example/algosolved/common/config/webConfig.java
+++ src/main/java/com/example/algosolved/common/config/webConfig.java
@@ -1,8 +1,9 @@
 package com.example.algosolved.common.config;
 
-import org.springframework.context.annotation.*;
-import org.springframework.http.*;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
@@ -11,6 +12,15 @@ public class webConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("classpath:/templates/", "classpath:/static/css/");
 
+    }
+
+    //react와 통신 시 사용됨
+    //TODO: 추후 환경변수로 프로덕션과 다른 오리진을 가지도록 수정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 
 }
