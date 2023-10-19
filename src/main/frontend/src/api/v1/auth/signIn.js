9a6200ee297ec0d40e26748258cdf1cf5d150935
rha6780 <rha4578@gmail.com>
2023-10-19 12:20:55 +0900

    feat: proxy 설정 및 api url 수정 payload 전송시 JSON 파싱 추가

--- src/main/frontend/src/api/v1/auth/signIn.js
+++ src/main/frontend/src/api/v1/auth/signIn.js
@@ -3,7 +3,7 @@ import { ApiClient } from 'api/v1/common/apiClient';
 
 export const signIn = async (signInPayload: SignInPayload) => {
     try {
-        const { data } = await ApiClient.post(`api/v1/auth/signIn`, signInPayload);
+        const { data } = await ApiClient.post(`api/v1/auth/login`, JSON.stringify(signInPayload));
         return data;
     } catch (error) {
         return error;
--- src/main/frontend/src/api/v1/auth/signUp.js
+++ src/main/frontend/src/api/v1/auth/signUp.js
@@ -2,9 +2,9 @@ import { ApiClient } from 'api/v1/common/apiClient';
 //import { UserState, SignUpPayload } from '../../../models/User';
 
 
-export const signUp = async (signUpPayload) => {
+export const signUp = async (signInPayload) => {
     try {
-        const { data } = await ApiClient.post(`api/v1/auth/signUp`, signUpPayload);
+        const { data } = await ApiClient.post(`api/v1/auth/signup`, JSON.stringify(signInPayload));
         return data;
     } catch (error) {
         return error;
